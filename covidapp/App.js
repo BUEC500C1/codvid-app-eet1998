88c36fda26edd22f06a322859deab8c7fd73c99b
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-04-23 10:20:48 -0400

    Created full-screen map and button

--- covidapp/App.js
+++ covidapp/App.js
@@ -1,48 +1,87 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  AlertIOS,
+} from 'react-native';
+import MapView, {Marker, AnimatedRegion} from 'react-native-maps';
 
+const screen = Dimensions.get('window');
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
+export default class App extends Component {
 
-export default class App extends React.Component {
   render() {
     return (
-      <MapView
-         style={{ flex: 1 }}
-         provider={PROVIDER_GOOGLE}
-         showsUserLocation
-         initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-      />
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          ref={ref => {this.map = ref;}}
+          initialRegion={{
+            latitude: 37.600425,
+            longitude: -122.385861,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+        >
+          {this.createMarkers()}
+        </MapView>
+        <View pointerEvents="none" style={styles.members}>
+          {this.createMembers()}
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.fitToMarkersToMap()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Fit Markers Onto Map</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
 
-export default HelloWorldApp;
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+    marginRight: 20,
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+    marginBottom: 400,
+  },
+  members: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: '100%',
+    paddingHorizontal: 10,
   },
 });
