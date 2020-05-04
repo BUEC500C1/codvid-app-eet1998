9630e1789013199c99f33371611f05848a6ae2bb
Erin Thomas <erinthomas737@gmail.com>
2020-05-04 13:56:28 -0400

    Added file via upload

---
+++ data.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://api.covid19api.com/countries"
+
+payload = {}
+headers= {}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+}
